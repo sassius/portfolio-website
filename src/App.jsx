8e1000ef50033b8c1539c50fc5492c3099686463
sassius <145041644+sassius@users.noreply.github.com>
2025-04-06 15:08:16 +0530

    empty initialised

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { BrowserRouter } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import About from "./components/About"
+import Experience from "./components/Experience"
+import Works from './components/Works';
+import Feedbacks from './components/Feedbacks';
+import Contact from './components/Contact'
+import Tech from "./components/Tech";
+import  StarsCanvas  from "./components/canvas/Stars"
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="z-0 relative bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar></Navbar>
+          <Hero></Hero>
+        </div>
+        <About></About>
+        <Experience></Experience>
+        <Tech></Tech>
+        <Works></Works>
+        <Feedbacks></Feedbacks>
+        <div className="z-0 relative">
+          <Contact></Contact>
+          <StarsCanvas></StarsCanvas>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
