3978c30b9f25cfc038587237df609dc13ccab02a
sassius <145041644+sassius@users.noreply.github.com>
2025-04-14 13:42:20 +0530

    done

--- src/assets/index.js
+++ src/assets/index.js
@@ -19,12 +19,15 @@ import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
+import threejs from "./tech/threejs.png";
 
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
+import initiator from "./company/initiator.png"
+import hmari from  "./company/hmari.png"
+import aicte from "./company/aicte.png"
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -55,6 +58,9 @@ export {
   meta,
   shopify,
   starbucks,
+  initiator,
+  aicte,
+  hmari, 
   tesla,
   carrent,
   jobit,
